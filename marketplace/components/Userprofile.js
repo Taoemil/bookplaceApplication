e50513ee92c8d056b9b91faa482c81016737fcce
Taoemil <taoemil@hotmail.com>
2023-10-22 13:07:41 +0200

    user profile message

--- marketplace/components/Userprofile.js
+++ marketplace/components/Userprofile.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
 
-const UserProfile = ({ navigation }) => {
+const UserProfile = () => {
+  const [bio, setBio] = useState('');
+  const [user, setUser] = useState(null);
+  const db = getFirestore();
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        const docRef = doc(db, 'users', currentUser.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setBio(docSnap.data().bio);
+        } else {
+          console.log("No such document!");
+        }
+      } else {
+        setUser(null);
+      }
+    });
+  }, [db]);
+
+  const handleSave = async () => {
+    if (user) {
+      const userRef = doc(db, 'users', user.uid);
+      await setDoc(userRef, { bio: bio }, { merge: true });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Here is your profile!</Text>
-        <Button title="Go to homescreen" onPress={() => navigation.navigate('Home')} />
+      <Text>Email: {user?.email}</Text>
+      <TextInput
+        value={bio}
+        onChangeText={setBio}
+        placeholder="Tell something about yourself"
+        style={styles.input}
+      />
+      <Button title="Save" onPress={handleSave} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 20,
   },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginVertical: 10,
   },
 });
 
